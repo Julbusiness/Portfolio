81be7f2fed8f3d3ed5fdd2595cda33de10e64e99
Julbusiness <julien.dutruch@yahoo.fr>
2023-10-08 18:09:43 +0200

    creation d'un fichier data pour rendre les pages dynamiques + configuration de la page home dynamique

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,29 @@
+import { useState, useEffect } from "react";
+import data from "../../data/data.json";
 import { Link } from "react-router-dom";
 import desktop from "../../assets/images/accueil/desktop1.jpg";
-import accueil from "../../assets/images/accueil/accueil.jpg";
-import apropos from "../../assets/images/accueil/apropos.jpg";
-import skills from "../../assets/images/accueil/skills.jpg";
-import projet from "../../assets/images/accueil/projet.jpg";
-import confiance from "../../assets/images/accueil/confiance.jpg";
-import contact from "../../assets/images/accueil/contact.jpg";
 import Title from "../../components/Title/Title";
 import "./Home.css";
 
 export default function Home() {
+	const currentData = data.map((item) => item.homeData.links);
+
+	let content;
+
+	content = currentData.map(
+		(item, index) => (
+			console.log(item),
+			item.map((link, index) => (
+				<div className='home-card' key={link.id}>
+					<Link to={link.route} className='link'>
+						<img src={`/assets/images/accueil/${link.image}`} alt='' />
+						<h3 className='title-card'>{link.title}</h3>
+					</Link>
+				</div>
+			))
+		)
+	);
+
 	return (
 		<section className='home'>
 			<div className='image-container'>
@@ -22,47 +36,10 @@ export default function Home() {
 				<div className='profession'>Développeur Front-End</div>
 			</div>
 			<div className='card-container'>
-			<div className="home-title-start">
-				<Title title="Transformez vos idées en réalités digitales"/>
-			</div>
-				<div className='home-card-container'>
-					<div className='home-card'>
-						<Link to='/' className='link'>
-							<img src={accueil} alt='' />
-							<h3 className='title-card'>Accueil</h3>
-						</Link>
-					</div>
-					<div className='home-card'>
-						<Link to='/apropos' className='link'>
-							<img src={apropos} alt='' />
-							<h3 className='title-card'>À propos</h3>
-						</Link>
-					</div>
-					<div className='home-card'>
-						<Link to='/skills' className='link'>
-							<img src={skills} alt='' />
-							<h3 className='title-card'>Skills</h3>
-						</Link>
-					</div>
-					<div className='home-card'>
-						<Link to='/projets' className='link'>
-							<img src={projet} alt='' />
-							<h3 className='title-card'>Projets</h3>
-						</Link>
-					</div>
-					<div className='home-card'>
-						<Link to='/confiance' className='link'>
-							<img src={confiance} alt='' />
-							<h3 className='title-card'>Confiance</h3>
-						</Link>
-					</div>
-					<div className='home-card'>
-						<Link to='/contact' className='link'>
-							<img src={contact} alt='' />
-							<h3 className='title-card'>Contact</h3>
-						</Link>
-					</div>
+				<div className='home-title-start'>
+					<Title title='Transformez vos idées en réalités digitales' />
 				</div>
+				<div className='home-card-container'>{content}</div>
 			</div>
 		</section>
 	);
