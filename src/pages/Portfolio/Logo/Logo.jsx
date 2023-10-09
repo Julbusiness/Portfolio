3c7306929dfb46221e4741a7b0b9d74b96d5e6e4
Julbusiness <julien.dutruch@yahoo.fr>
2023-10-09 08:49:03 +0200

    création de la page logo dynamique avec la bdd

--- src/pages/Portfolio/Logo/Logo.jsx
+++ src/pages/Portfolio/Logo/Logo.jsx
@@ -1,14 +1,34 @@
 import Bandeau from "../../../components/Bandeau/Bandeau";
 import imageBandeau from "/assets/images/bandeaux-sticky/bandeau-sticky6.png";
 import "./Logo.css";
+import Title from "../../../components/Title/Title";
+import data from "../../../data/data.json";
+import { Link } from "react-router-dom";
 
 export default function Logo() {
-  return (
-    <section className="logo">
-      <Bandeau image={imageBandeau} />
-      <div className="logo-container">
+	const logoData = data.map((item) => item.portfolioData[0].logo);
 
-      </div>
-    </section>
-  )
+	let content;
+	content = logoData.map((item, index) =>
+		item.map((card, index) => (
+			<img src={`/assets/images/logos/${card.image}`} alt='' key={card.id} />
+		))
+	);
+
+	return (
+		<section className='logo'>
+			<Bandeau image={imageBandeau} />
+			<div className='logo-container'>
+				<Link to="/portfolio">
+					<button>
+						<i className='bx bx-left-arrow-alt'></i>
+					</button>
+				</Link>
+				<div className='title-start'>
+					<Title title='Logo' />
+				</div>
+				<div className='logos'>{content}</div>
+			</div>
+		</section>
+	);
 }
