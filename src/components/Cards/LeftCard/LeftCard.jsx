d8ee72c8f11296a6b2af580a8033300d0216f4e9
Julbusiness <julien.dutruch@yahoo.fr>
2023-10-25 19:54:00 +0200

    responsive mobile pour la page skills ok

--- src/components/Cards/LeftCard/LeftCard.jsx
+++ src/components/Cards/LeftCard/LeftCard.jsx
@@ -7,7 +7,6 @@ export default function LeftCard(props) {
 				<img src={props.image} alt='' />
 			</div>
 			<div className='left-card-text'>
-				<div className='tag'>{props.tag}</div>
 				<h2>{props.title}</h2>
 				<p className='leftCard-text'>{props.text}</p>
 				<p className='leftCard-project'>
